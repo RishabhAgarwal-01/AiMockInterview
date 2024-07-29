669e18f1b6951b5d8c36f159491c0a1df913f50f
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-30 00:50:34 +0530

    modification to payment link

--- app/dashboard/upgrade/page.jsx
+++ app/dashboard/upgrade/page.jsx
@@ -43,6 +43,8 @@ function Upgrade() {
 
               <a
                 href={item.price === 0 ? '/' : item.link}
+                target={item.price === 0 ? undefined : "_blank"} // Open payment link in a new tab
+                rel={item.price === 0 ? undefined : "noopener noreferrer"} // Security for new tab links
                 className="mt-8 block rounded-full border border-indigo-600 bg-white px-12 py-3 text-center text-sm font-medium text-indigo-600 hover:ring-1 hover:ring-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
               >
                 {item.price === 0 ? 'Get Started' : 'Subscribe Now'}
