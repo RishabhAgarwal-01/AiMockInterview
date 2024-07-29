4bc2b84bcae5f75a538bce11ac5926da6996f1ff
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-29 13:22:36 +0530

    added speech to text adn text to speech functionality in answer page

--- app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
+++ app/dashboard/interview/[interviewId]/start/_components/QuestionsSection.jsx
@@ -1,8 +1,19 @@
-import { Lightbulb } from 'lucide-react'
+import { Lightbulb, Volume2 } from 'lucide-react'
 import React from 'react'
 
 function QuestionsSection({mockInterviewQuestion, activeQuestionIndex}) {
 // console.log(mockInterviewQuestion[activeQuestionIndex])
+
+const texttoSpeech = (text)=>{
+  if('speechSynthesis' in window){
+    const speech= new SpeechSynthesisUtterance(text);
+    window.speechSynthesis.speak(speech);
+  }
+  else{
+    alert('Sorry, your browser does not support text-to-speech')
+  }
+}
+
   return mockInterviewQuestion && (
     <div className='pt-5 px-1 py-2 border rounded-lg bg-white shadow-md my-10'>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
@@ -16,6 +27,7 @@ function QuestionsSection({mockInterviewQuestion, activeQuestionIndex}) {
           )}
         </div>
         <h2 className='my-5 md:text-lg'>{mockInterviewQuestion[activeQuestionIndex]}</h2>
+        <Volume2 className="cursor-pointer" onClick={()=>texttoSpeech(mockInterviewQuestion[activeQuestionIndex])}/>
         <div className='border rounded-lg p-4 bg-blue-100 mt-20'>
             <h2 className='flex gap-2 items-center text-primary'>
                 <Lightbulb/>
