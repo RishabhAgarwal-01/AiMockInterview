750673fa8e6de0af172f780a9a5c726a401e3f4f
RishabhAgarwal-01 <agarwalrishabh964@gmail.com>
2024-07-30 23:28:24 +0530

    fix some bugs

--- app/dashboard/interview/[interviewId]/feedback/page.jsx
+++ app/dashboard/interview/[interviewId]/feedback/page.jsx
@@ -69,7 +69,7 @@ function Feedback({params}) {
            <div className='flex- flex-col gap-2'>
               <h2 className='text-red-500 p-2 rounded-lg'><strong>Rating: </strong>{item.rating}</h2>
               <h2 className='p-2 border rounded-lg bg-red-50 text-sm text-red-900'><strong>Your Answer: </strong>{item.userAns}</h2>
-              <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer: </strong>{item.userAns}</h2>
+              <h2 className='p-2 border rounded-lg bg-green-50 text-sm text-green-900'><strong>Correct Answer: </strong>{item.correctAns}</h2>
               <h2 className='p-2 border rounded-lg bg-blue-50 text-sm text-primary'><strong> feedback: </strong>{item.feedback}</h2>
            </div>
         </CollapsibleContent>
